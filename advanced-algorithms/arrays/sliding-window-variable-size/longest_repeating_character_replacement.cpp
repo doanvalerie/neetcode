33fb67f9bd1e4c1147a83c0d7fde261ae8756cb9
Valerie Doan <valeriephuongdoan@gmail.com>
2024-10-30 12:59:44 -0700

    Review: Advanced Algorithms - Longest Repeating Character Replacement

--- advanced-algorithms/arrays/sliding-window-variable-size/longest_repeating_character_replacement.cpp
+++ advanced-algorithms/arrays/sliding-window-variable-size/longest_repeating_character_replacement.cpp
@@ -43,3 +43,69 @@ int characterReplacement(string s, int k)
 
     return result;
 }
+
+/* -------------------- REVIEW -------------------- */
+
+/*
+ * Author: Valerie Doan
+ * Date: October 30, 2024
+ * Problem: Longest Repeating Character Replacement
+ * Level: Medium
+ * Neetcode Course: Advanced Algorithms
+ * Neetcode Topic: Sliding Window Variable Size
+ */
+
+int characterReplacement(string s, int k)
+{
+    unordered_map<char, int> char_freq;
+    int highest_freq = 0;
+    int res = 0;
+    int L = 0;
+
+    for (int R = 0; R < s.size(); R++)
+    {
+        highest_freq = max(highest_freq, ++char_freq[s[R]]);
+
+        if (R - L + 1 - highest_freq <= k)
+        {
+            res = max(res, R - L + 1);
+        }
+        else
+        {
+            char_freq[s[L++]]--;
+        }
+    }
+
+    return res;
+}
+
+int characterReplacement_v2(string s, int k)
+{
+    unordered_map<char, int> char_freq;
+    int highest_freq = 0;
+    int res = 0;
+    int L = 0;
+
+    for (int R = 0; R < s.size(); R++)
+    {
+        if (++char_freq[s[R]] > highest_freq)
+        {
+            highest_freq = char_freq[s[R]];
+        }
+        if (R - L + 1 - highest_freq <= k)
+        {
+            res = max(res, R - L + 1);
+        }
+
+        while (R - L + 1 - highest_freq > k)
+        {
+            char_freq[s[L++]]--;
+            for (const auto &[key, value] : char_freq)
+            {
+                highest_freq = max(highest_freq, value);
+            }
+        }
+    }
+
+    return res;
+}
