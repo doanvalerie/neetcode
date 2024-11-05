5e99a83eb56c26168f72c170f5bc45dbf9d017e1
Valerie Doan <valeriephuongdoan@gmail.com>
2024-11-04 16:19:32 -0800

    Review: Neetcode 150 – Permutation in String

--- nc-150/sliding-window/permutation_in_string.cpp
+++ nc-150/sliding-window/permutation_in_string.cpp
@@ -46,3 +46,40 @@ bool checkInclusion(string s1, string s2)
 
     return false;
 }
+
+/* -------------------- REVIEW -------------------- */
+
+/*
+ * Author: Valerie Doan
+ * Date: November 2, 2024
+ * Problem: Permutation in String
+ * Level: Medium
+ * Neetcode Course: Neetcode 150
+ * Neetcode Topic: Sliding Window
+ */
+
+bool checkInclusion(string s1, string s2)
+{
+    unordered_map<char, int> char_freq;
+    for (char ch : s1)
+    {
+        char_freq[ch]++;
+    }
+
+    int L = 0;
+    for (int R = 0; R < s2.size(); R++)
+    {
+        char_freq[s2[R]]--;
+        while (char_freq[s2[R]] < 0)
+        {
+            char_freq[s2[L]]++;
+            L++;
+        }
+        if (R - L + 1 == s1.size())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
